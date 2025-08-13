4fb65f413a5456d349c8c61c4221b68927234b0b
adh11200878 <adh11200878@gmail.com>
2025-08-13 10:55:29 +0800

    傳入值修正

--- src/views/login.vue
+++ src/views/login.vue
@@ -4,47 +4,17 @@
       <v-col cols="12" sm="8" md="6" lg="4">
         <v-card elevation="10" class="pa-6 rounded-lg">
           <v-card-title class="text-h5 text-center mb-4">
-            <v-icon
-              icon="mdi-account-circle"
-              size="48"
-              color="primary"
-              class="mb-2"
-            />
+            <v-icon icon="mdi-account-circle" size="48" color="primary" class="mb-2" />
             <div>歡迎登入</div>
           </v-card-title>
           <v-card-text>
-            <v-text-field
-              v-model="username"
-              label="帳號"
-              prepend-icon="mdi-account"
-              variant="outlined"
-              class="mb-4"
-            />
-            <v-text-field
-              v-model="password"
-              label="密碼"
-              type="password"
-              prepend-icon="mdi-lock"
-              variant="outlined"
-              class="mb-4"
-              @keyup.enter="login"
-            />
-            <v-btn
-              @click="login"
-              color="primary"
-              size="large"
-              block
-              rounded
-              class="mb-2"
-            >
+            <v-text-field v-model="username" label="帳號" prepend-icon="mdi-account" variant="outlined" class="mb-4" />
+            <v-text-field v-model="password" label="密碼" type="password" prepend-icon="mdi-lock" variant="outlined"
+              class="mb-4" @keyup.enter="login" />
+            <v-btn @click="login" color="primary" size="large" block rounded class="mb-2">
               登入
             </v-btn>
-            <v-alert
-              v-if="errorMessage"
-              type="error"
-              variant="tonal"
-              class="mt-4"
-            >
+            <v-alert v-if="errorMessage" type="error" variant="tonal" class="mt-4">
               {{ errorMessage }}
             </v-alert>
           </v-card-text>
@@ -65,12 +35,12 @@ const password = ref("");
 const errorMessage = ref("");
 const router = useRouter();
 
-const loginData: LoginModel = {
-  username: username.value,
-  password: password.value,
-};
-
 const login = async () => {
+  //傳入值
+  const loginData: LoginModel = {
+    username: username.value,
+    password: password.value,
+  };
   errorMessage.value = "";
   const success = await authLogin(loginData);
   if (success) {
