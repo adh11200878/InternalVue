9e3759fe47ee123841fd359f933e79cb6f65e424
adh11200878 <adh11200878@gmail.com>
2025-08-13 14:02:38 +0800

    reactive 改回 ref

--- src/views/login.vue
+++ src/views/login.vue
@@ -8,13 +8,15 @@
             <div>歡迎登入</div>
           </v-card-title>
           <v-card-text>
-            <v-text-field v-model="loginData.username" label="帳號" prepend-icon="mdi-account" variant="outlined"
-              class="mb-4" />
-            <v-text-field v-model="loginData.password" label="密碼" type="password" prepend-icon="mdi-lock"
-              variant="outlined" class="mb-4" @keyup.enter="login" />
-            <v-btn @click="login" color="primary" size="large" block rounded class="mb-2">
-              登入
-            </v-btn>
+            <v-form ref="formRef">
+              <v-text-field v-model="loginData.username" label="帳號" prepend-icon="mdi-account" variant="outlined"
+                class="mb-4" :rules="rules.a" />
+              <v-text-field v-model="loginData.password" label="密碼" type="password" prepend-icon="mdi-lock"
+                variant="outlined" class="mb-4" @keyup.enter="login" :rules="rules.a" />
+              <v-btn @click="login" color="primary" size="large" block rounded class="mb-2">
+                登入
+              </v-btn>
+            </v-form>
             <v-alert v-if="errorMessage" type="error" variant="tonal" class="mt-4">
               {{ errorMessage }}
             </v-alert>
@@ -26,22 +28,33 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { authLogin } from "../services/auth";
 import type { LoginModel } from "../models/LoginModel";
 
-const errorMessage = ref("");
+
 const router = useRouter();
 
-const loginData: LoginModel = reactive({
+//Vuetify 表單驗證
+const rules = {
+  a: [(v: string) => !!v || "不可為空"],
+};
+//錯誤訊息
+const errorMessage = ref("");
+//from驗證 判斷所有的(:rules)
+const formRef = ref();
+const loginData = ref<LoginModel>({
   username: '',
   password: ''
-});
-
+})
+//登入
 const login = async () => {
+  if (!formRef.value?.validate()) {
+    return;
+  }
   errorMessage.value = "";
-  const success = await authLogin(loginData);
+  const success = await authLogin(loginData.value);
   if (success) {
     router.push("/content");
   } else {
