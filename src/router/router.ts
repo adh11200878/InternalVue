0fb1959b522ae21dd5cf4569885d16f0f556030d
adh11200878 <adh11200878@gmail.com>
2025-08-14 11:30:40 +0800

    配置使用@指向src資料夾

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,26 +3,26 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    component: () => import("../layouts/DefaultLayout.vue"),
+    component: () => import("@/layouts/DefaultLayout.vue"),
     meta: { requiresAuth: true }, //經過路由守衛
     children: [
       {
         path: "",
-        component: () => import("../views/index.vue"),
+        component: () => import("@/views/index.vue"),
       },
       {
         path: "content",
-        component: () => import("../views/content.vue"),
+        component: () => import("@/views/content.vue"),
       },
     ],
   },
   {
     path: "/login",
-    component: () => import("../layouts/EmptyLayout.vue"),
+    component: () => import("@/layouts/EmptyLayout.vue"),
     children: [
       {
         path: "",
-        component: () => import("../views/login.vue"),
+        component: () => import("@/views/login.vue"),
       },
     ],
   },
